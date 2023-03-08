ccc41582b325e29824e5219c7a1548cfd65cc2df
Sean Deitrich <seantdeitrich@gmail.com>
2023-03-07 20:24:16 -0500

    Added Text and Vector2 Movement

--- game.py
+++ game.py
@@ -2,22 +2,20 @@ import sys
 import random
 import math
 import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, GAME_NAME, NAME
 from utility import *
-#----------------------------------------------------------------------------
-# Create/Adjust Constants Here
-#----------------------------------------------------------------------------
-WIDTH = 500
-HEIGHT = 500
-NAME = "Sean Deitrich"
-GAME_NAME = "Sean's Game"
 #----------------------------------------------------------------------------
 # Create Colors Here
 #----------------------------------------------------------------------------
 #The three numbers in the parens are the red, green, and blue values
 #They are always on a scale of 0 to 255. Google "Color Picker"!
+
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+MAGENTA = (255, 0, 255)
+CYAN = (0, 255, 255)
+YELLOW = (255, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
@@ -25,12 +23,20 @@ WHITE = (255, 255, 255)
 # ▼▼▼ DO NOT ADJUST THIS CODE ▼▼▼ 
 #----------------------------------------------------------------------------
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.key.set_repeat() #Controls how held keys are repeated (delay, interval) -- See PyGame docs
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SCALED, vsync=1)
 pygame.display.set_caption(GAME_NAME)
 clock = pygame.time.Clock()
 #----------------------------------------------------------------------------
 # ▲▲▲ DO NOT ADJUST THIS CODE ▲▲▲
 #----------------------------------------------------------------------------
+
+#----------------------------------------------------------------------------
+# Create Text Boxes Here
+#----------------------------------------------------------------------------
+#This creates a text box, but you'll have to display it in the level you want
+
+titleText = Text(screen, "Title Screen", BLACK, 30,100,100)
 
 #----------------------------------------------------------------------------
 # Create Backgrounds Here
@@ -40,9 +46,11 @@ level1Background = "landscape.jpg"
 #----------------------------------------------------------------------------
 # Create Objects, Characters, Enemies, etc. Here
 #----------------------------------------------------------------------------
-r = Block(RED, 100, 100, 100, 100)#Color, Width, Height, X Location, Y Location
-g = Block(GREEN, 100, 100, 150, 100)
-b = Block(BLUE, 100, 100, 200, 100)
+redblock = Block(RED, 100, 100, 100, 100)#Color, Width, Height, X Location, Y Location
+redblock.set_speed(10) #Set the speed of the red block when we control it
+
+greenblock = Block(GREEN, 100, 100, 150, 100)
+blueblock = Block(BLUE, 100, 100, 200, 100)
 
 #----------------------------------------------------------------------------
 # Create Levels Here
@@ -53,8 +61,8 @@ b = Block(BLUE, 100, 100, 200, 100)
 titleSprites = [] 
 #Step 2. Create the Level itself
 titleScreen = Level(screen, titleScreenBackground, titleSprites)
-
-level1Sprites = [r,g,b]
+#Here's another example
+level1Sprites = [redblock, greenblock, blueblock]
 level1 = Level(screen, level1Background, level1Sprites)
 
 #----------------------------------------------------------------------------
@@ -67,6 +75,12 @@ timer = Timer()
 #----------------------------------------------------------------------------
 game = Game(titleScreen) #Create a game with the starting level as titleScreen
 gameRunning = True #Ensure the game is running
+
+def checkQuit(event):
+    if event.type == pygame.QUIT: #If the close (X) button is pressed
+        gameRunning = False #The game ends
+        pygame.quit() #PyGame Quits
+        sys.exit() #The window exits
 #----------------------------------------------------------------------------
 # ▲▲▲ DO NOT ADJUST THIS CODE ▲▲▲ 
 #----------------------------------------------------------------------------
@@ -74,17 +88,38 @@ gameRunning = True #Ensure the game is running
 #----------------------------------------------------------------------------
 # Create Controls for Each Level Here
 #----------------------------------------------------------------------------
-def titleControls(event):
-    if event.type == pygame.KEYDOWN: #When any key is pressed down
-        if event.key == pygame.K_SPACE: #If it was the space key
-            game.setLevel(level1) #Change the game to level1
-            timer.start() #Start the timer
+def titleControls(keys_pressed):
+    titleText.display()
+    for event in pygame.event.get(): #Check to see whats happening in the game
+        checkQuit(event) #Check to see if the user quit the game, should be in every level
+        if event.type == pygame.KEYDOWN: #When any key is pressed down
+            if event.key == pygame.K_SPACE: #If it was the space key
+                game.setLevel(level1) #Change the game to level1
+                timer.start() #Start the timer
 
-def level1Controls(event):
-    if event.type == pygame.KEYDOWN: #When any key is pressed down
-        if event.key == pygame.K_SPACE: #If it was the space key
-            print("Hit Space in Level 1: " + str(timer.time())) #Print that we hit space in level 1 and the time on the timer
+def level1Controls(keys_pressed):
+    direction = pygame.math.Vector2() #Don't worry about what this is for now
+    #This method is used to check if keys were JUST pressed
+    for event in pygame.event.get(): #Check to see whats happening in the game
+        checkQuit(event) #Check to see if the user quit the game, should be in every level
+        if event.type == pygame.KEYDOWN: #When any key is pressed down
+            if event.key == pygame.K_SPACE: #If it was the space key
+                print("Hit Space in Level 1: " + str(timer.time())) #Print that we hit space in level 1 and the time on the timer
+            if event.key == pygame.K_t:
+                timer.restart()
+        
+    #This method is used to check to see when keys are HELD
+    if keys_pressed[pygame.K_d]:
+        direction.x = 1
+    if keys_pressed[pygame.K_a]:
+        direction.x = -1
+    if keys_pressed[pygame.K_w]:
+        direction.y = -1
+    if keys_pressed[pygame.K_s]:
+        direction.y = 1
 
+    #Enable movement for the block (player)
+    redblock.move(direction)
 
 #----------------------------------------------------------------------------
 # ▼▼▼ DO NOT ADJUST THIS CODE ▼▼▼ 
@@ -92,25 +127,23 @@ def level1Controls(event):
 while gameRunning: #While the game is running
     screen.fill(RED) #Fill the screen with red
     clock.tick(60) #Runs the game at 60FPS
-    for event in pygame.event.get(): #Check to see whats happening in the game
-        if event.type == pygame.QUIT: #If the close (X) button is pressed
-            gameRunning = False #The game ends
-            pygame.quit() #PyGame Quits
-            sys.exit() #The window exits
+    keys_pressed = pygame.key.get_pressed()
+    game.run() #Display the current level in the game
 #----------------------------------------------------------------------------
 # ▲▲▲ DO NOT ADJUST THIS CODE ▲▲▲
 #----------------------------------------------------------------------------
 #----------------------------------------------------------------------------
 # Input Processing for Each Level Goes Here 
 #----------------------------------------------------------------------------
-        elif game.currentLevel == titleScreen:
-            titleControls(event)
-        elif game.currentLevel == level1:
-            level1Controls(event)
+    if game.currentLevel == titleScreen:
+        titleControls(keys_pressed)
+
+
+    elif game.currentLevel == level1:
+        level1Controls(keys_pressed)
 
 #----------------------------------------------------------------------------
 # ▼▼▼ DO NOT ADJUST THIS CODE ▼▼▼ 
 #----------------------------------------------------------------------------
-        game.run() #Display the current level in the game
-        pygame.display.update() #Update the display every frame
+    pygame.display.update() #Update the display every frame
 
--- utility.py
+++ utility.py
@@ -1,4 +1,6 @@
 import pygame
+import pygame.math
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, GAME_NAME, NAME
 class Projectile(pygame.sprite.Sprite):
     '''TODO Projectile Class'''
     def __init__(self, position, direction):
@@ -12,13 +14,36 @@ class Block(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface([width, height])
         self.image.fill(color)
-        self.position = (x,y)
+        self.position = pygame.math.Vector2(x,y)
         self.rect = self.image.get_rect()
+        self.speed = 10
     
+    def set_speed(self, speed)
+        self.speed = speed
+
     def set_position(self, x, y):
-        self.position = (x,y)
+        self.position = pygame.math.Vector2(x,y)
         self.rect = self.image.get_rect()
     
+    def move(self, direction):
+        if direction.length() != 0:
+            direction = direction.normalize()
+            velocity = direction * self.speed
+            self.position += velocity
+
+class Text():
+    def __init__(self, screen, text, color, size, x=0, y=0):
+        self.screen = screen
+        self.position = [x,y]
+        newfont = pygame.font.SysFont("Arial",size,True,False)
+        self.text = newfont.render(text,True,color)
+    
+    def display(self):
+        self.screen.blit(self.text, self.position)
+
+    def set_position(self, x, y):
+        self.position = [x,y]
+
 class Character(pygame.sprite.Sprite):
     '''A class for each character in your game'''
     def __init__(self, image, x=0, y=0):
@@ -36,11 +61,12 @@ class Level():
     def __init__(self, screen, background, sprites):
         self.sprites = pygame.sprite.Group()
         self.background = pygame.image.load("./images/"+background).convert()
+        self.background = pygame.transform.scale(self.background, (SCREEN_WIDTH, SCREEN_HEIGHT))
         self.screen = screen
         for sprite in sprites:
             self.sprites.add(sprite)
 
-    def display(self):
+    def display(self): 
         #Display this levels background
         self.screen.blit(self.background, (0,0))
         #Display every sprite at its own position
@@ -52,14 +78,21 @@ class Timer():
         self.startTime = 0
 
     def start(self):
+        self.enabled = True
         self.startTime = pygame.time.get_ticks()
     
     def restart(self): #This is the same as start, but allows for easier reading of code in the main file
+        self.enabled = True
         self.startTime = pygame.time.get_ticks()
 
     def time(self):
-        return (pygame.time.get_ticks() - self.startTime)/1000
+        if self.enabled:
+            return round((pygame.time.get_ticks() - self.startTime)/1000, 1)
+        return 0
     
+    def end(self):
+        self.enabled = False
+
 class Game():
     def __init__(self, startingLevel):
         self.currentLevel = startingLevel
